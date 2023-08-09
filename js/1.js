4d7e91065064ad9352b82dc123a0359adc3105c2
RuqayaAnwer <ruqayaanwer5@gmail.com>
2023-08-09 20:26:55 +0300

    my portfolio

--- js/1.js
+++ js/1.js
@@ -1,3 +1,42 @@
+
+
+const menu = document.getElementById("menu");
+const action = document.getElementById("action");
+
+menu.addEventListener("click", () => {
+    hundleMenu();
+})
+function hundleMenu() {
+   
+    menu.classList.toggle("is-active");
+    action.classList.toggle("is-active");
+
+    
+}
+/*----------------------------skills--------------------- */
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 window.onscroll = () => {
     let header = document.querySelector('.header');
 
